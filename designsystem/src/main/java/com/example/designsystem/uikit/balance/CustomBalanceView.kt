6c74fe64bf2355a287ada412d23fcb149aabfbed
Victor Bitencourt <victor.bitencourt@vhbsolution.com.br>
2021-08-21 13:13:32 -0300

    initial commit

--- designsystem/src/main/java/com/example/designsystem/uikit/balance/CustomBalanceView.kt
+++ designsystem/src/main/java/com/example/designsystem/uikit/balance/CustomBalanceView.kt
@@ -5,9 +5,12 @@ import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.View
 import android.view.View.inflate
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.constraintlayout.widget.ConstraintSet
+import androidx.core.view.allViews
+import androidx.core.view.forEach
 import com.example.designsystem.R
 import com.facebook.shimmer.ShimmerFrameLayout
 import java.text.NumberFormat
@@ -19,20 +22,42 @@ class CustomBalanceView @JvmOverloads constructor(
     defStyle: Int = 0
 ) : ConstraintLayout(context, attrs, defStyle) {
 
-    private val tvBalance by lazy{ findViewById<TextView>(R.id.tvBalance) }
-    private val shimmer by lazy{ findViewById<ShimmerFrameLayout>(R.id.shimmer_view_container) }
+    private val tvTitle by lazy { findViewById<TextView>(R.id.tvTitle) }
+    private val tvBalance by lazy { findViewById<TextView>(R.id.tvBalance) }
+    private val shimmer by lazy { findViewById<ShimmerFrameLayout>(R.id.shimmer_view_container) }
+    private val ivError by lazy { findViewById<ImageView>(R.id.imageView) }
 
     init {
-        LayoutInflater.from(context).inflate( R.layout.custom_balance_view, this, true)
+        LayoutInflater.from(context).inflate(R.layout.custom_balance_view, this, true)
     }
 
-    fun showBalance(balance: Double) {
-        tvBalance.text  = NumberFormat.getCurrencyInstance(Locale.getDefault()).format(balance)
+    fun setBalance(balance: Double){
+        tvBalance.text = NumberFormat.getCurrencyInstance(Locale.getDefault()).format(balance)
+    }
+
+    fun showBalance() {
+        hideView()
         tvBalance.visibility = View.VISIBLE
     }
 
-    fun showLoading(isLoading: Boolean){
+    fun showLoading(isLoading: Boolean) {
+        hideView()
         shimmer.visibility = if (isLoading) View.VISIBLE else View.GONE
+    }
+
+    fun showError(action: () -> Unit) {
+        hideView()
+        tvBalance.text = "Error ao carregar"
+        ivError.setOnClickListener { action() }
+
+        tvBalance.visibility = View.VISIBLE
+        ivError.visibility = View.VISIBLE
+    }
+
+    private fun hideView() {
+        tvBalance.visibility = View.GONE
+        ivError.visibility = View.GONE
+        shimmer.visibility = View.GONE
     }
 
 }
